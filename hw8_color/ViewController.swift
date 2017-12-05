4a2dde7e77a42541f7a2130d90f8dabcdb9e82f0
建基吳 <lukewu@jianjiwudeMacBook-Pro.local>
2017-12-05 20:11:45 +0800

    Initial Commit

---
+++ hw8_color/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  hw8_color
+//
+//  Created by 建基吳 on 2017/12/5.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
